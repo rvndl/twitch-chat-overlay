e890b3fc10b1fabd9a896efcf2ef8c8a94cce648
rvndl <rvndldev@gmail.com>
2022-09-05 19:34:25 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="Q_yP03gI2rE6U195WodXLoQ5c7cctUVc_gAL9R92Uto"
+        />
+      </Head>
       <NextSeo
         title="Twitch Chat Overlay"
         description="Twitch Chat Overlay makes you able to embed your Twitch chat during the broadcast."
@@ -29,10 +36,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           site: "@rvn_dl",
           cardType: "summary_large_image",
         }}
-      />
-      <meta
-        name="google-site-verification"
-        content="Q_yP03gI2rE6U195WodXLoQ5c7cctUVc_gAL9R92Uto"
       />
       <Toaster />
       <Component {...pageProps} />
