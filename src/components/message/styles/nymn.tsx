539f0cddf54ae3d617216b13c4f45ffe840e0f87
rvndl <rvndldev@gmail.com>
2022-11-16 15:42:14 +0100

    christmas style

--- src/components/message/styles/nymn.tsx
+++ src/components/message/styles/nymn.tsx
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
-import { useMemo } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useMemo, useState } from "react";
 import { Message } from "../../../interfaces/message";
 import { lightenColor, shadeColor } from "../../../utils";
 import { MessageWithEmotes } from "../message-emotes";
@@ -12,23 +12,69 @@ interface Props {
 }
 
 export const NymNStyle = ({ message: { user, message }, showNames }: Props) => {
+  const [played, setPlayed] = useState(false);
   const userColor = lightenColor(user.color);
   const shadedColor = shadeColor(userColor, 30);
 
   const vanish = vanishCommands.some((command) => message.includes(command));
-  // const image = useMemo(
-  //   () => (Math.random() >= 0.3 ? "/nymn-halloween.png" : "/nymn-skeleton.png"),
-  //   []
-  // );
+  const image = useMemo(() => {
+    if (user.mod) {
+      return "/grinch-christmas.png";
+    }
+
+    if (user.badges?.vip) {
+      return "/vip-christmas.png";
+    }
+
+    return "/nymn-christmas.png";
+  }, [user]);
+
+  const snowball = message.startsWith("!snowball");
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    if (snowball) {
+      timeout = setTimeout(() => setPlayed(true), 1700);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [snowball]);
 
   return (
     <div className="flex mb-2">
       <div
-        className="w-8 h-8 shrink-0"
+        className="relative w-8 h-8 shrink-0"
         style={{
           background: `linear-gradient(0deg, ${shadedColor} 0%, ${userColor} 100%)`,
         }}
       >
+        <AnimatePresence>
+          {snowball && !played && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.5 }}
+              exit={{ opacity: 0 }}
+            >
+              <motion.img
+                initial={{ rotate: -200 }}
+                animate={{ rotate: [-200, -200, 0] }}
+                transition={{ delay: 0.7 }}
+                src="/glove.png"
+                className="absolute z-10 -top-1 -right-3"
+                alt=""
+              />
+              <motion.img
+                initial={{ right: 0 }}
+                animate={{ right: "-100vw", opacity: 0.5 }}
+                transition={{ delay: 1.2, duration: 1.4 }}
+                src="/snowball.png"
+                className="absolute right-0 w-5 -top-1 animate-spin"
+                alt=""
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
         <motion.img
           animate={
             vanish && {
@@ -38,7 +84,7 @@ export const NymNStyle = ({ message: { user, message }, showNames }: Props) => {
             }
           }
           transition={{ delay: 1, duration: 2 }}
-          src="/nymn.png"
+          src={image}
           className="w-8 h-8 shrink-0"
         />
       </div>
