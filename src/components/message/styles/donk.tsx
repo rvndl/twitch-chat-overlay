192f652b9bb8834d5e32127ead68d0b5d10578cb
rvndl <rvndldev@gmail.com>
2023-08-03 22:24:45 +0200

    fix: Lighten username color

--- src/components/message/styles/donk.tsx
+++ src/components/message/styles/donk.tsx
@@ -1,5 +1,5 @@
 import { Message } from "../../../interfaces/message";
-import { lightenColor } from "../../../utils";
+import { lightenColor, shadeColor } from "../../../utils";
 import { Donk } from "../../icons";
 import { MessageWithEmotes } from "../message-emotes";
 
@@ -10,14 +10,15 @@ interface Props {
 
 export const DonkStyle = ({ message: { user, message }, showNames }: Props) => {
   const userColor = lightenColor(user.color);
+  const shadedColor = shadeColor(userColor, 30);
 
   return (
     <div className="flex mb-2">
-      <div className="shrink-0" style={{ color: userColor }}>
+      <div className="shrink-0" style={{ color: shadedColor }}>
         <Donk />
       </div>
       {showNames && (
-        <p className="flex ml-1 shrink-0" style={{ color: userColor }}>
+        <p className="flex ml-1 shrink-0" style={{ color: shadedColor }}>
           {user["display-name"]}:
         </p>
       )}
